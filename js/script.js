3ba8b0a390e4e2512c2296ab1e2f47ac4ee9ef20
Benjamin Normann Hansen <normanndesigns@outlook.com>
2018-12-09 00:03:46 +0100

    Added notification button and fixed part of the terminal design

--- js/script.js
+++ js/script.js
@@ -1,4 +1,8 @@
-var terminal = document.getElementById('terminal');
+var terminal = document.getElementById('terminal'), 
+notificationBTN = document.getElementById('notificationBTN'), 
+notificitaionsModule = document.getElementById('notificitaionsModule'),
+terminalWrapper = document.getElementById('terminalWrapper'),
+terminalInfo = document.getElementById('terminalInfo');
 
 window.addEventListener('load', function() {
     SpatialNavigation.init();
@@ -8,25 +12,36 @@ window.addEventListener('load', function() {
     SpatialNavigation.makeFocusable();
     SpatialNavigation.focus();
 });
+notificationBTN.addEventListener('click', function(){
+    onOrOff(notificitaionsModule);
+});
+terminalWrapper.addEventListener('click', function(){
+    terminalInfo.focus();
+});
 
-function show(object){
-    object.className = "flex navigate";
+function onOrOff(object){
+    if(object.className == "notificitaionsModule flex"){
+        display(object, "notificitaionsModule none");
+    }else if(object.className == "notificitaionsModule none"){
+        display(object, "notificitaionsModule flex");
+    }
 }
-function hide(object){
-    object.className = "none";
+
+function display(object, className){
+    object.className = className;
 }
 
 function search(){
     var sidepages = ['portfolio', 'cv', 'facebook', 'linkedin', 'instagram', 'contact', 'help'];
     for(i = 0; i < sidepages.length; i++){
-        hide(document.getElementById(sidepages[i]));
+        display(document.getElementById(sidepages[i]), "none");
     }
     var terminalInput = document.getElementById('terminalInfo').value;
     terminalInput = terminalInput.toLowerCase();
     if(terminalInput !== ""){
         for(i = 0; i < sidepages.length; i++){
             if(sidepages[i].startsWith(terminalInput)){
-                show(document.getElementById(sidepages[i]));
+                display(document.getElementById(sidepages[i]), "flex navigate");
                 document.getElementById(sidepages[i]).tabIndex = i;
             } 
         }
